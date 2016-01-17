dd819b8fd96e9a2b8529c21ee9a5c63825ec319f
xiaoleeza <yongheng2520@sina.com>
2016-01-17 17:49:35 +0800

    初始化

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.accessDataSet = new WindowsFormsApplication2.accessDataSet();
+            this.tbGradeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tb_GradeTableAdapter = new WindowsFormsApplication2.accessDataSetTableAdapters.tb_GradeTableAdapter();
+            this.btn2txt = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.accessDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGradeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // accessDataSet
+            // 
+            this.accessDataSet.DataSetName = "accessDataSet";
+            this.accessDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tbGradeBindingSource
+            // 
+            this.tbGradeBindingSource.DataMember = "tb_Grade";
+            this.tbGradeBindingSource.DataSource = this.accessDataSet;
+            // 
+            // tb_GradeTableAdapter
+            // 
+            this.tb_GradeTableAdapter.ClearBeforeFill = true;
+            // 
+            // btn2txt
+            // 
+            this.btn2txt.Location = new System.Drawing.Point(136, 282);
+            this.btn2txt.Name = "btn2txt";
+            this.btn2txt.Size = new System.Drawing.Size(75, 23);
+            this.btn2txt.TabIndex = 1;
+            this.btn2txt.Text = "导出到TXT";
+            this.btn2txt.UseVisualStyleBackColor = true;
+            this.btn2txt.Click += new System.EventHandler(this.btn2txt_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(449, 246);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(478, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(120, 246);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 335);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn2txt);
+            this.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.accessDataSet, "tb_Grade.name", true));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.accessDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGradeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private accessDataSet accessDataSet;
+        private System.Windows.Forms.BindingSource tbGradeBindingSource;
+        private accessDataSetTableAdapters.tb_GradeTableAdapter tb_GradeTableAdapter;
+        private System.Windows.Forms.Button btn2txt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        //全局变量,文件全路径
+        private string stFilePath = string.Empty;
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds = new DataSet();
+        string str_connection = "SERVER=127.0.0.1,8829;DATABASE=uestc;UID=sa;PWD=593106";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //订单数据表Orders,数据查询语句
+        string str_select = "select * from tiku831 order by pbNo";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“accessDataSet.tb_Grade”中。您可以根据需要移动或删除它。
+            this.tb_GradeTableAdapter.Fill(this.accessDataSet.tb_Grade);
+
+        }
+
+        /// <summary>
+        /// 导出数据到txt文件.<br></br>
+        /// 2009-04-24 YJ 定义函数.<br></br>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btn2txt_Click(object sender, EventArgs e)
+        {
+            //此处的文本文件在工程下Bin的程序集目录下
+            stFilePath = Application.StartupPath.Trim() + "\\students" + DateTime.Now.ToString("yyyy年MM月dd日hh时mm分ss秒") + ".tex";
+            StreamWriter swStream;
+            if (File.Exists(stFilePath))
+            {
+                swStream = new StreamWriter(stFilePath);
+            }
+            else
+            {
+                swStream = File.CreateText(stFilePath);
+            }
+
+            swStream.Write("% !Mode:: \"TeX:UTF-8\"\r\n");
+
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                string _strTemp = "\\newproblem{" + ds.Tables[0].Rows[i][0].ToString().Substring(4) + "}{" + ds.Tables[0].Rows[i][3].ToString() + "\r\n}" + "{\r\n" + ds.Tables[0].Rows[i][4].ToString() + "\r\n}\r\n\r\n";
+                swStream.Write(_strTemp);
+            }
+            //关闭流,释放资源
+            swStream.Flush();
+            swStream.Close();
+            //导入Txt文件后,自动打开文件
+            Process.Start("notepad++.exe", stFilePath);
+ 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                //初始化清空数据集ds
+                ds.Clear();
+                con = new SqlConnection(str_connection);
+                //打开数据连接
+                con.Open();
+                da = new SqlDataAdapter(str_select, con);
+                //填充数据集到ds
+                da.Fill(ds);
+                //绑定数据到DataGridView1
+                this.dataGridView1.DataSource = ds.Tables[0].DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
